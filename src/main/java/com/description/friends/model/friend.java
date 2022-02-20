b7eacfb5559c1f5a3ba6da790467e38517f071b0
greglacinto@gmail.com <greglacinto@gmail.com>
2022-02-20 17:18:54 +0100

    added model

---
+++ src/main/java/com/description/friends/model/friend.java
@@ -0,0 +1,70 @@
+package com.description.friends.model;
+
+public class friend {
+    private String id;
+    private String name;
+    private String dob;
+    private String email;
+    private String familyName;
+    private String location;
+    private String career;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+}
+
+
